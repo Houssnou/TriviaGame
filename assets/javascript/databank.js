f3444e8162ec2410f6634009080e2a0deb99356f
Houssnou <hosni.moubarak@gmail.com>
2018-12-20 22:02:47 -0500

    full databank.js

---
+++ assets/javascript/databank.js
@@ -0,0 +1,458 @@
+var databank = [{
+  question: "Name A Number That is Associated With Being Lucky Or Unlucky",
+  choices: ["7", "13", "3"],
+  answer: 1
+},
+
+{
+  question: "What is the capital of United Kingdom?",
+  choices: ["Manchester", "Birmingham", "London"],
+  answer: 2
+},
+
+{
+  question: "Name The Most Used Piece Of Furniture In A House.",
+  choices: ["	Couch	", "	Bed	", "	Arm Chair	"],
+  answer: 1
+},
+
+{
+  question: "Name A Job In Which An Attractive Person Probably Makes Better Tips.	",
+  choices: ["	Server	", "	Exotic Dancer	", "	Bartender	"],
+  answer: 1
+},
+{
+  question: "Name A Prop used By Tap Dancers	",
+  choices: ["	Cane	", "	Top Hat	", "	Baton	"],
+  answer: 1
+},
+{
+  question: "If You Drew Homer Simpson ? s Name In A Secret Santa Exchange, What Would You Buy Him ?",
+  choices : ["	Beer	", "	Donuts	", "	Bowling Ball	"],
+  answer : 1
+},
+{
+  question: "	Name Something You Do To An Item Before Giving It As A Gift	",
+  choices: ["	Wrap It	", "	Remove Price Tag	", "	Buy It	"],
+  answer: 1
+},
+{
+  question: "	Name A Place Where It Might Be Romantic To Get Stranded With Your Partner	",
+  choices: ["	Island/Beach	", "	Park	", "	Cabin/Lodge	"],
+  answer: 1
+},
+{
+  question: "	Name A Good Gift For Someone Who Is Always Late.	",
+  choices: ["	Watch	", "	Alarm Clock	", "	Calendar	"],
+  answer: 1
+},
+{
+  question: "	Why Might A Family Move Into A Bigger House?	",
+  choices: ["	Family Has Grown	", "	Want More Space	", "	Can Afford More	"],
+  answer: 1
+},
+{
+  question: " 'Other Than Movie Tickets, Name Something A Crowded Movie Theatre Might Run Out Of.' ",
+  choices: ["	Popcorn	", "	Space	", "	Candy	"],
+  answer: 1
+},
+{
+  question: "	Name Something You Should Probably Book Ahead Of Time If You?re Going On Vacation.	",
+  choices: ["	Hotel	", "	Flight	", "	Car	"],
+  answer: 1
+},
+{
+  question: "	Name Something People Bet On	",
+  choices: ["	Horses Races	", "	Sports	", "	Card Games	"],
+  answer: 1
+},
+{
+  question: "	Name A Character Who?s Known For Almost Ruining Christmas.	",
+  choices: ["	Grinch	", "	Ebenezer Scrooge	", "	Charlie Brown	"],
+  answer: 1
+},
+{
+  question: "	Name A City Where You?d Hate The Long Commute To Work.	",
+  choices: ["	New York City	", "	Los Angeles	", "	Chicago	"],
+  answer: 1
+},
+{
+  question: "	Name Something That?s Not Allowed In Some Hotels.	",
+  choices: ["	Pets	", "	Smoking	", "	Children	"],
+  answer: 1
+},
+{
+  question: "	Name Something That Might Happen If You Eat While Driving.	",
+  choices: ["	Crash	", "	Spill	", "	Choke	"],
+  answer: 1
+},
+{
+  question: "If Adults Trick Or Treated, What Might They Request Instead Of Candy ?",
+  choices : ["	Money	", "	Alcohol	", "	Cigarettes	"],
+  answer: 1
+},
+{
+  question: "	Name A Word Or Phrase Used For Cooking On The Grill	",
+  choices: ["	Barbecue	", "	Grilling	", "	Cookout	"],
+  answer: 1
+},
+{
+  question: "	What Might You Ask To Borrow From Someone At The Laundromat?	",
+  choices: ["	Detergent	", "	Change	", "	Dryer Sheets	"],
+  answer: 1
+},
+{
+  question: "	Which Festive Holiday Was More Fun When You Were A Kid?	",
+  choices: ["	Christmas	", "	Halloween	", "	Easter	"],
+  answer: 1
+},
+{
+  question: "	What Would You Be Surprised To Hear That A Married Couple Does Separately?	",
+  choices: ["	Sleep	", "	Vacation	", "	Eat Meals	"],
+  answer: 1
+},
+{
+  question: "	Name A Reason You Wouldn?t Want To Live Next Door To A Movie Star	",
+  choices: ["	Paparazzi	", "	Loud Parties	", "	Fans	"],
+  answer: 1
+},
+{
+  question: " On A Resume, What Might A Person Claim They Can Do Quickly ? ",
+  choices : ["	Type	", "	Learn	", "	Finish Tasks	"],
+  answer: 1
+},
+{
+  question: "	Name Something That Parent Of A Baby Is Constantly Listening For	",
+  choices: ["	Crying	", "	Breathing	", "	First Words	"],
+  answer: 1
+},
+{
+  question: " Other Than Boxing, Name A Sport In Which You ? re Likely To See Athletes Fighting ",
+  choices : ["	Hockey	", "	Wrestling	", "	Football	"],
+  answer: 1
+},
+{
+  question: "	Name Something Nice You Get Sick Of Saying During The Holidays	",
+  choices: ["	Merry Christmas	", "	Thank You	", "	Great To See You	"],
+  answer: 1
+},
+{
+  question: "	Name A Place Where People Wear Very Little Clothing During The Winter	",
+  choices: ["	Vacation	", "	Home	", "	Gym	"],
+  answer: 1
+},
+{
+  question: "	Name A Phrase That Often Appears In Children?S Stories	",
+  choices: ["	Once Upon A Time	", "	The End	", "	Happily Ever After	"],
+  answer: 1
+},
+{
+  question: "	What Do People Say At Halloween That Would Be Inappropriate To Hear At Christmas?	",
+  choices: ["	Trick Or Treat	", "	Boo	", "	Happy Halloween	"],
+  answer: 1
+},
+{
+  question: "	Name A Drink Usually Associated With Christmas	",
+  choices: ["	Egg Nog	", "	Cider	", "	Hot Chocolate	"],
+  answer: 1
+},
+{
+  question: "	Name Something Tha Can Be Pumped	",
+  choices: ["	Gas	", "	Tire	", "	Iron	"],
+  answer: 1
+},
+{
+  question: "Other Than Illness, Name An Acceptable Excuse For Missing Work.",
+  choices: ["	Funeral	", "	Car Trouble	", "	Jury Duty	"],
+  answer: 1
+},
+{
+  question: "	Who In Your Life Is Most Likely To Brad About You?	",
+  choices: ["	Parent	", "	Partner	", "	Kids	"],
+  answer: 1
+},
+{
+  question: "	We Asked 100 Wives: Name A Place Where You Wouldn?t Want Your Husband To Go With His Buddies.	",
+  choices: ["	Gentleman?s Club	", "	The Bar	", "	Las Vegas	"],
+  answer: 1
+},
+{
+  question: "	Name A Crime That Some People Probably Commit Every Day.	",
+  choices: ["	Speeding	", "	Jaywalking	", "	Littering	"],
+  answer: 1
+},
+{
+  question: "Name Something A Grown Woman Might Lie About, But A Young Girl Wouldn't",
+  choices : ["	Age	", "	Weight	", "	Love Life	"],
+  answer: 1
+},
+{
+  question: "	Name A Decoration You Might Find Inside A Mexican Restaurant	",
+  choices: ["	Sombrero	", "	Pinata	", "	Maracas	"],
+  answer: 1
+},
+{
+  question: "	What Might You See Inside A Bag Of Chips That Would Make You Stop Eating Them?	",
+  choices: ["	Hair	", "	Bug	", "	Mold	"],
+  answer: 1
+},
+{
+  question: "	Name Something Tourists Point Up At	",
+  choices: ["	Tall Buildings	", "	Statues/ Monuments	", "	Mountains	"],
+  answer: 1
+},
+{
+  question: " What Do Some People Do Immediately After Breaking Up, And Others Take Time To Do ? ",
+  choices : ["	Date Again	", "	Mourn	", "	Move On	"],
+  answer: 1
+},
+{
+  question: "	Name Something Considerate That People Often Do For You In An Elevator	",
+  choices: ["	Hold Door	", "	Push Button	", "	Move Over	"],
+  answer: 1
+},
+{
+  question: "If You Live In The Country, Name A Type Of Lights That You Don ? t See At Night.",
+  choices : ["	Street Lights	", "	Traffic	", "	Neon Light	"],
+  answer: 1
+},
+{
+  question: "	Name Something You Might Add Milk To In The Morning.	",
+  choices: ["	Cold Cereal	", "	Coffee	", "	Oatmeal	"],
+  answer: 1
+},
+{
+  question: " Name Something Children Wear, But Adults Don't",
+  choices : ["	Diaper	", "	Bib	", "	Overalls	"],
+  answer: 1
+},
+{
+  question: "	Name Something That A Demanding Bride May Ask Her Maid-Of-Honor To do That Could End Their Friendship.	",
+  choices: ["	Wear An Ugly Dress	", "	Lie For Her	", "	Pay For Dress	"],
+  answer: 1
+},
+{
+  question: "	Name A Profession Where You Might Have A Dog For A Sidekick	",
+  choices: ["	Police Officer	", "	Firefighter	", "	Performer	"],
+  answer: 1
+},
+{
+  question: "	We Asked 100 Single People: Who In Your Life Gives You The Most Pressure To Settle Down and Marry?	",
+  choices: ["	Mom	", "	Friends	", "	Person I?m Dating	"],
+  answer: 1
+},
+{
+  question: "	Name A Form Of Communication That Few People Use Anymore.	",
+  choices: ["	Written Letters	", "	Telegraph	", "	Phone	"],
+  answer: 1
+},
+{
+  question: "	Name A Profession That Has To Worry About Getting Sued	",
+  choices: ["	Doctor	", "	Lawyer	", "	Police Officer	"],
+  answer: 1
+},
+{
+  question: "	Name Something You Need To Eat Before It Melts.	",
+  choices: ["	Ice Cream	", "	Chocolate	", "	Popsicle	"],
+  answer: 1
+},
+{
+  question: "	Name A prize You May Win From A Carnival Game.	",
+  choices: ["	Stuffed Animal	", "	Gold Fish	", "	Poster	"],
+  answer: 1
+},
+{
+  question: "	Name Something Drivers Often Do If They Spot A Police Car Up Ahead	",
+  choices: ["	Slow Down	", "	Move Over	", "	Put Seatbelt On	"],
+  answer: 1
+},
+{
+  question: "	Name An Animal That You See More Of In The City.	",
+  choices: ["	Cat	", "	Pigeon	", "	Rat	"],
+  answer: 1
+},
+{
+  question: "	Name Something Kids Might Collect From The Beach	",
+  choices: ["	Shells	", "	Sand	", "	Rocks	"],
+  answer: 1
+},
+{
+  question: "	Name Something It Would Be Rude Not To Do When Being Introduced To A Stranger	",
+  choices: ["	Shake Hands	", "	Smile	", "	Say Hello	"],
+  answer: 1
+},
+{
+  question: "Name A Skill That's More Important For Teens To Learn Today, Than Previously ",
+  choices : ["	Computer Skills	", "	Driving	", "	Second Language	"],
+  answer: 1
+},
+{
+  question: "	Name Something People Wear To Avoid A Sunburn	",
+  choices: ["	Sunscreen	", "	Hat	", "	Shirt/Coverup	"],
+  answer: 1
+},
+{
+  question: "	Name A Specific Person You?d Ask To Help You Move If You Were In A Jam.	",
+  choices: ["	Friend	", "	Parent	", "	Sibling	"],
+  answer: 1
+},
+{
+  question: "	Name Something Parents Of Twins Do That Makes Their Children Seem Even More Similar.	",
+  choices: ["	Dress Alike	", "	Hair Style	", "	Similar Names	"],
+  answer: 1
+},
+{
+  question: " Other Than To Buy Books, Why Might Someone Go To A Bookstore ? ",
+  choices : ["	Read	", "	Coffee	", "	Socialize	"],
+  answer: 1
+},
+{
+  question: "	What Do You Find Out About A Town By Reading Signs On The Side Of The Road?	",
+  choices: ["	Population	", "	Town Name	", "	Speed Limit	"],
+  answer: 1
+},
+{
+  question: "	Name A Profession That?s Only Successful If People Are Laughing At Them.	",
+  choices: ["	Comedian	", "	Clown	", "	Sports Mascot	"],
+  answer: 1
+},
+{
+  question: "	Tell Me A Reason People Give For Wearing Black.	",
+  choices: ["	Funeral	", "	Slimming	", "	Style	"],
+  answer: 1
+},
+{
+  question: "If An Alien Gave You A Guided Tour Of Outer Space, What Would Be Must - See Stops ? ' ",
+  choices : ["	Other Planets	", "	Moon	", "	Sun	"],
+  answer: 1
+},
+{
+  question: "	What Might A Parent Videotape Their Child Doing Around The Christmas Holiday?	",
+  choices: ["	Opening Presents	", "	Being In Pageant	", "	Visiting Santa	"],
+  answer: 1
+},
+{
+  question: "	What Is A Color That You Would See On A Country?s Flag?	",
+  choices: ["	Red	", "	Blue	", "	White	"],
+  answer: 1
+},
+{
+  question: "	Name A Song That Has ?Christmas? In The Title	",
+  choices: ["	White Christmas	", "	Oh Christmas Tree	", "	Wish You Merry Xmas	"],
+  answer: 1
+},
+{
+  question: "	Name Something Santa Does That You wouldn?t want Your House guest To Do.	",
+  choices: ["	Come Down Chimney	", "	Eat Your Coookies	", "	Arrive Late at Night	"],
+  answer: 1
+},
+{
+  question: "Other Than Displays Of Affection, Name Something You Wish Couples Wouldn ? t Do In Public.",
+  choices : ["	Fight	", "	Feed Each Other	", "	Baby Talk	"],
+  answer: 1
+},
+{
+  question: "	Name A Reason You Might Dread Going To A New Year?s Party At A Club.	",
+  choices: ["	Crowded	", "	No Date	", "	Don?t Drink	"],
+  answer: 1
+},
+{
+  question: "	Which Color Would You Be Surprised To See A Bride Wearing On Her Wedding Day?	",
+  choices: ["	Black	", "	Red	", "	Green	"],
+  answer: 1
+},
+{
+  question: "	Name Something You?D Hate To Find Floating In Your Coffee	",
+  choices: ["	Bug	", "	Hair	", "	Food Crumb	"],
+  answer: 1
+},
+{
+  question: " Name Something Santa Does, That You Wouldn ? t Want Your House Guests To Do.",
+  choices : ["	Come Down Chimney	", "	Eat Your Cookies	", "	Arrive Late At Nite	"],
+  answer: 1
+},
+{
+  question: "	Name A Specific Night Of The Year That You Hope To Be Kissed.	",
+  choices: ["	New Year Eve?s	", "	Valentine?s	", "	Christmas	"],
+  answer: 1
+},
+{
+  question: "	Name Something That You Can Get For Free At A Casino.	",
+  choices: ["	Drinks	", "	Food	", "	Entertainment	"],
+  answer: 1
+},
+{
+  question: "	Name A Person Who You?d Never Trust to Set You Up On A Blind Date	",
+  choices: ["	Mother	", "	Sibling	", "	Ex	"],
+  answer: 1
+},
+{
+  question: " If You See Pink Frosting On A Cake, What Flavor Would You Expect It To Be ",
+  choices: ["	Strawberry	", "	Cherry	", "	Raspberry	"],
+  answer: 1
+},
+{
+  question: "	Name Something You?d Take Camping But Be Arrested For If You Brought It To A Bank.	",
+  choices: ["	Gun	", "	Hatchet	", "	Alcohol	"],
+  answer: 1
+},
+{
+  question: " Before Visiting A New City, Name Something For Which You Might Get A Recommendation.",
+  choices: ["	Places To Stay	", "	Restaurant	", "	Attraction	"],
+  answer: 1
+},
+{
+  question: "	Name A Reason Why It?s Hard To Slow Dance With A Ghost.	",
+  choices: ["	Can?t See Them	", "	Nothing To Hold	", "	Can?t Lead	"],
+  answer: 1
+},
+{
+  question: "	Name A Decoration You Might Find Inside A Mexican Restaurant.	",
+  choices: ["	Sombrero	", "	Pinata	", "	Maracas	"],
+  answer: 1
+},
+{
+  question: "	Name Something A Dad Would Look Silly Borrowing From His Child At The Bowling Alley.	",
+  choices: ["	Shoes	", "	Lightweight Ball	", "	Money	"],
+  answer: 1
+},
+{
+  question: "	Name A Famous Robert Who Many Women Consider Handsome.	",
+  choices: ["	Robert Redford	", "	Robert De Niro	", "	Robert Kennedy	"],
+  answer: 1
+},
+{
+  question: "	Name An Article Of Clothing That Most People Have 10 Of.	",
+  choices: ["	Pairs Of Shoes	", "	Pairs Of Socks	", "	Shirts	"],
+  answer: 1
+},
+{
+  question: "	Name Something A Man Might Do At The Gym To Impress A Woman.	",
+  choices: ["	Lift Weights	", "	Flex	", "	Take Off Shirt	"],
+  answer: 1
+},
+{
+  question: "	Name Something People Wait In Line For On Hot Days	",
+  choices: ["	Ice Cream	", "	Cold Drinks	", "	Water Park	"],
+  answer: 1
+},
+{
+  question: "	Name Something A Contestant In A Beauty Pageant Might Wear	",
+  choices: ["	Bathing Suit	", "	Gown	", "	Makeup	"],
+  answer: 1
+},
+{
+  question: "	Name A Kind Of Cookie People Love To Dunk In Milk	",
+  choices: ["	Chocolate Chip	", "	Oreo	", "	Oatmeal	"],
+  answer: 1
+},
+{
+  question: "	Name A Night Of The Week When People Stay Up Later Than Usual	",
+  choices: ["	Friday	", "	Saturday	", "	Sunday	"],
+  answer: 1
+},
+{
+  question: "	'If There Was An Award For ?Loudest In The Zoo,? Which Animal Would Win It?'	",
+  choices: ["	Lion	", "	Elephant	", "	Monkey	"],
+  answer: 1
+}];
